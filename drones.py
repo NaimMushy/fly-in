e97bdd59de5b5c3b963be371a8414c72c7a0c5fa
NaimMushy <bady.iris@gmail.com>
2026-04-19 20:54:30 +0200

    trying to put together a drone logic

--- drones.py
+++ drones.py
@@ -1,11 +1,12 @@
 class Drone:
 
-    def __init__(self, drone_id: int, start_pos: tuple[int, int]) -> None:
+    def __init__(self, drone_id: int, start_pos: tuple[int, int], path_to_follow: Path) -> None:
 
         self.id: int = drone_id
         self.pos: tuple[int, int] = start_pos
         self.waiting: bool = False
         self.next_dest: tuple[int, int] = start_pos
+        self.path_to_follow: Path = path_to_follow
 
 
 """
@@ -34,14 +35,15 @@ class Pathfinder:
         self.paths: list[Path] = self.calculate_paths(
             self.map.start_hub,
             Path(),
-            self.map.end_hub
+            self.map.end_hub,
+            []
         )
 
-    def calculate_paths(self, current_hub: Zone, current_path: Path, dest: Zone) -> list[Path]:
+    def calculate_paths(self, current_hub: Zone, current_path: Path, dest: Zone, possible_paths: list[Path]) -> list[Path]:
 
         if current_hub.zone_type == "blocked":
 
-            return self.paths
+            return possible_paths
 
         current_path.cost += 1
 
@@ -51,8 +53,33 @@ class Pathfinder:
 
         if current_hub == dest:
 
-            return self.paths + current_path
+            return possible_paths + current_path
 
         for branch in current_path.connections:
 
-            return self.calculate_paths(branch, current_path, dest)
+            return self.calculate_paths(branch, current_path, dest, possible_paths)
+
+    def reevaluate_drone_path(self, drone: Drone) -> None:
+
+        possible_paths: list[Path] = self.calculate_paths(
+            drone.current_zone,
+            Path(),
+            self.map.end_hub,
+            []
+        )
+
+        for path in possible_paths:
+
+            if len(path.path) >= 2 and path.path[1].occupied == path.path[1].max_drones:
+                path.cost += 1
+
+        if len(drone.path_to_follow.path) >= 2 and drone.path_to_follow.path[1].occupied == drone.path_to_follow.path[1].max_drones:
+            drone.path_to_follow.cost += 1
+
+        for path in possible_paths:
+
+            if path.priority > drone.path_to_follow.priority:
+                drone.path_to_follow = path
+
+            elif path.cost < drone.path_to_follow.cost:
+                drone.path_to_follow = path
