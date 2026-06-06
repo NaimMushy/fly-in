de2d81ac29d3bf2036d19cd4e64fa224950a237a
NaimMushy <bady.iris@gmail.com>
2026-06-06 17:41:50 +0200

    keeping old display in my personal repo (to remove for the vogsphere repo)

---
+++ old_display/fly_in.py
@@ -0,0 +1,274 @@
+import sys
+import os
+import time
+from srcs import MapParser, Map, PathFinder, DroneMonitor, TuiDisplay, ArcadeDisplay
+
+
+DEFAULT_MAP: str = "maps/easy/01_linear_path.txt"
+
+
+MAP_DIR: str = "maps"
+
+
+MAPS: dict[str, list[str]] = {
+    "easy": ["01_linear_path", "02_simple_fork", "03_basic_capacity"],
+    "medium": ["01_dead_end_trap", "02_circular_loop", "03_priority_puzzle"],
+    "hard": ["01_maze_nightmare", "02_capacity_hell", "03_ultimate_challenge"],
+    "challenger": ["01_the_impossible_dream"]
+}
+
+
+def main() -> None:
+
+    """
+
+    Launches a simulation of drone routing
+    using pathfinding algorithm and a tui display.
+
+    """
+
+    map_parser: MapParser = MapParser()
+
+    if len(sys.argv) > 2:
+        print("Too many arguments for the program!")
+        return
+
+    map_file: str = DEFAULT_MAP
+
+    if len(sys.argv) == 2:
+        map_file = sys.argv[1]
+
+    arcade_mode: bool = False
+    user_input: str = ""
+    ret: int = 0
+
+    while user_input != "q" and ret != -1:
+
+        try:
+
+            os.system('clear')
+
+            TuiDisplay.display_menu(arcade_mode, map_file)
+
+            user_input = input()
+
+            while user_input not in ["s", "q", "l", "m", "g"] and user_input:
+                print("Invalid command!")
+                time.sleep(0.4)
+                os.system('clear')
+                TuiDisplay.display_menu(arcade_mode, map_file)
+                user_input = input()
+
+            if user_input == "s":
+                map_file = input("\nEnter the path to the map file: ")
+
+            elif user_input == "g":
+                arcade_mode = not (arcade_mode)
+                os.system('clear')
+                print(
+                    f"{'Deactivated' if not arcade_mode else 'Activated'} "
+                    "graphic representation mode!"
+                )
+                time.sleep(1)
+
+            elif user_input == "m":
+                os.system('clear')
+                TuiDisplay.display_maps(MAPS, MAP_DIR)
+
+            elif user_input == "l":
+                os.system('clear')
+                ret = launch_drones(
+                    map_parser,
+                    map_file,
+                    arcade_mode
+                )
+
+        except KeyboardInterrupt:
+
+            return
+
+
+def launch_drones(
+    map_parser: MapParser,
+    map_file: str,
+    arcade_mode: bool
+) -> int:
+
+    """
+
+    Goes through every step of the simulation
+    and saves it in a specific state.
+
+    Parameters
+    ----------
+    map_parser : MapParser
+        The parser used to verify and validate map data.
+    map_file : str
+        The name of the file containing all of the map data.
+    arcade_mode : int
+        Indicates whether or not the graphic representation mode is activated.
+
+    """
+
+    os.system('clear')
+    drone_map: Map | None = map_parser.parse_map(map_file)
+
+    if not drone_map:
+        print(f" ✘ Map '{map_file}' refused : Invalid data\n")
+        input("Press any key to continue...")
+        return 0
+
+    elif not PathFinder.calculate_paths(
+        drone_map.start_hub, drone_map.end_hub
+    ):
+        print(f" ✘ Map '{map_file}' refused : No paths found\n")
+        input("Press any key to continue...")
+        return 0
+
+    drone_monitor: DroneMonitor = DroneMonitor(
+        drone_map,
+    )
+
+    if arcade_mode:
+
+        display: ArcadeDisplay = ArcadeDisplay(drone_map.hubs)
+        display.add_state(drone_map.hubs, drone_map.connections, [], "")
+
+    while drone_monitor.drones:
+
+        drone_monitor.update_drones()
+
+        if arcade_mode:
+            display.add_state(
+                drone_map.hubs,
+                drone_map.connections,
+                drone_monitor.drones_delivered,
+                drone_monitor.current_turn_log
+            )
+
+    if arcade_mode:
+        display.start_visu()
+
+    TuiDisplay.display_end(drone_monitor.turns, drone_monitor.avg)
+
+    return 1
+
+#         new_state: State = State(info_mode, tui_display.console)
+#         new_state.turn = 0
+#         new_state.display_map = tui_display.map_updated([])
+#         new_state.zones_occupied[drone_map.start_hub.name] = [
+#             d.id for d in drone_map.start_hub.occupied
+#         ]
+#         cur_states: list[State] = [new_state]
+# 
+#         while drone_monitor.drones:
+# 
+#             new_state = State(info_mode, tui_display.console)
+#             drone_monitor.update_drones(new_state)
+#             new_state.display_map = tui_display.map_updated((
+#                 []
+#                 if drone_map.end_hub.name
+#                 not in new_state.zones_occupied.keys()
+#                 else new_state.zones_occupied[drone_map.end_hub.name]
+#             ))
+#             cur_states.append(new_state)
+# 
+#         if map_file not in states.keys():
+#             states[map_file] = []
+# 
+#         states[map_file].append((
+#             cur_states,
+#             drone_monitor.turns,
+#             drone_monitor.avg,
+#             info_mode
+#         ))
+# 
+#     time.sleep(1)
+#     for state_list in states[map_file]:
+# 
+#         if state_list[3] and info_mode:
+#             return show_states(tui_display, state_list)
+# 
+#         elif not info_mode and not state_list[3]:
+#             return show_states(tui_display, state_list)
+# 
+#     return 0
+# 
+# 
+# def show_states(
+#     tui_display: TuiDisplay,
+#     states: tuple[list[State], int, int, int]
+# ) -> int:
+# 
+#     """
+# 
+#     Displays the simulation steps
+#     based on the user's commands.
+# 
+#     Parameters
+#     ----------
+#     tui_display : TuiDisplay
+#         The display object used to display the user options and steps.
+#     states : tuple[list[State], int, int, int]
+#         All of the states pertaining to the current simulation
+#         being run.
+# 
+#     """
+# 
+#     cur_state: int = 0
+# 
+#     os.system('clear')
+#     tui_display.display_state(states[0][cur_state], states[1])
+# 
+#     user_input: str = ""
+# 
+#     while user_input != "m":
+# 
+#         try:
+# 
+#             user_input = input()
+# 
+#             while user_input not in ["n", "p", "m"] and user_input:
+#                 print("Invalid command!")
+#                 time.sleep(0.4)
+#                 os.system('clear')
+#                 tui_display.display_state(states[0][cur_state], states[1])
+#                 user_input = input()
+# 
+#             if user_input == "n":
+# 
+#                 os.system('clear')
+#                 if cur_state == len(states[0]) - 1:
+#                     tui_display.display_end(states[3], states[1], states[2])
+#                     input("Press any key to continue...")
+#                     user_input = "m"
+# 
+#                 else:
+#                     cur_state += 1
+#                     os.system('clear')
+#                     tui_display.display_state(states[0][cur_state], states[1])
+# 
+#             elif user_input == "p":
+# 
+#                 if cur_state == 0:
+# 
+#                     print("Invalid command!")
+#                     time.sleep(0.4)
+#                     os.system('clear')
+#                     tui_display.display_state(states[0][cur_state], states[1])
+# 
+#                 else:
+#                     cur_state -= 1
+#                     os.system('clear')
+#                     tui_display.display_state(states[0][cur_state], states[1])
+# 
+#         except KeyboardInterrupt:
+# 
+#             return -1
+# 
+#     return 0
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ old_display/state.py
@@ -0,0 +1,236 @@
+import time
+from rich import print
+from rich.text import Text
+from rich.console import Console
+
+
+class Char:
+
+    """
+
+    A class representing a character (a pixel) of the screen's grid.
+
+    """
+
+    def __init__(
+        self,
+        char: str,
+        style: str,
+        zone_name: str,
+        is_border: bool
+    ) -> None:
+
+        """
+
+        Initializes the attributes of a Char object.
+
+        Parameters
+        ----------
+        char : str
+            The character displayed on the screen.
+        style : str
+            The style (appearance) of the character.
+        zone_name: str
+            The name of the zone in which the Char is
+            (if not in a zone, the name is empty).
+        is_border: bool
+            A boolean indicating whether or not the Char is a border of a zone.
+
+        """
+
+        self.char: str = char
+        self.style: str = style
+        self.relation: str = zone_name
+        self.is_zone: bool = False
+        if self.relation:
+            self.is_zone = True
+        self.is_border: bool = is_border
+        self.connection_char: str = ""
+
+        self.parent: "Char"
+        self.dist_from_start: int = 0
+        self.dist_from_arrival: int = 0
+        self.dir_from_parent: tuple[int, int] | None = None
+        self.row: int = 0
+        self.col: int = 0
+
+    @property
+    def total_dist(self) -> int:
+
+        """
+
+        A property of the Char class
+        to calculate the Manhattan distance of the Char.
+
+        Returns
+        -------
+        int
+            The Manhattan distance of the Char object
+            (distance from start point + distance from end point).
+
+        """
+        return self.dist_from_start + self.dist_from_arrival
+
+    def update_dist(
+        self,
+        parent: "Char",
+        in_to_explore: bool,
+        goal: "Char"
+    ) -> None:
+
+        """
+
+        Updates the distance of the Char object.
+
+        Parameters
+        ----------
+        parent : Char
+            The parent of the Char (the one preceding it in the path).
+        in_to_explore : bool
+            A boolean indicating if the Char has to be explored.
+        goal: Char
+            The arrival point of the path.
+
+        """
+
+        new_dir: tuple[int, int] = (
+            self.row - parent.row,
+            self.col - parent.col
+        )
+        goal_dir: tuple[int, int] = (
+            0 if self.row == goal.row else (1 if goal.row > self.row else -1),
+            0 if self.col == goal.col else (1 if goal.col > self.col else -1)
+        )
+
+        turn_penalty: int = 0
+        if parent.dir_from_parent and parent.dir_from_parent != new_dir:
+            turn_penalty = 5
+
+        alignment_penalty: int = 0
+        if new_dir != goal_dir:
+            alignment_penalty = 1
+
+        new_dist: int = (
+            parent.dist_from_start
+            + 1
+            + turn_penalty
+            + alignment_penalty
+        )
+
+        if not in_to_explore or new_dist < self.dist_from_start:
+
+            self.dist_from_start = new_dist
+            self.parent = parent
+            self.dir_from_parent = new_dir
+
+
+class State:
+
+    """
+
+    A class representing a certain state during the simulation.
+
+    """
+
+    def __init__(
+        self,
+        info_mode: int,
+        console: Console
+    ) -> None:
+
+        """
+
+        Initializes the attributes of a State object.
+
+        Parameters
+        ----------
+        info_mode : int
+            Indicates whether or not the information mode is activated
+            (1 if yes, 0 otherwise).
+        console: Console
+            A console object from rich to be able to print text with styles.
+
+        """
+
+        self.display_map: list[list[Char]] = []
+        self.drone_moves: str = ""
+        self.nb_drone_moved: int = 0
+        self.turn: int = 0
+        self.zones_occupied: dict[str, list[int]] = {}
+        self.drones_delivered: list[int] = []
+        self.info_mode: int = info_mode
+        self.console: Console = console
+
+    def display_info(self) -> None:
+
+        """
+
+        Displays the entire map
+        and the information about the current turn.
+
+        """
+
+        print("\n\n")
+
+        for line in self.display_map:
+
+            for char in line:
+
+                char_to_print: str = char.char
+
+                if char.connection_char:
+                    char_to_print = char.connection_char
+
+                if char.style:
+                    self.console.print(Text(
+                        char_to_print,
+                        style=char.style
+                    ), end="")
+
+                else:
+                    print(char_to_print, end="")
+
+        time.sleep(0.3)
+
+        if not self.drone_moves and not self.info_mode:
+            return
+
+        if self.drone_moves:
+            print("\n==== DRONE MOVEMENTS ====\n")
+            print(f" ➤ {self.drone_moves}", end="")
+
+        print()
+
+        if self.info_mode != 0:
+
+            print("\n==== ADDITIONAL INFORMATION ====\n")
+
+            print(f" ➤ number of drones that moved: {self.nb_drone_moved}\n")
+
+            print(" ➤ zones currently occupied:", end="")
+
+            if len(self.zones_occupied) == 0:
+                print(" 0")
+            print("\n")
+
+            for name, occupying in self.zones_occupied.items():
+
+                print(f" {name}:", end="")
+
+                for drone_id in occupying:
+                    print(f" D{drone_id}", end="")
+
+                print("\n")
+
+            print(" ➤ drones already delivered:", end="")
+
+            if len(self.drones_delivered) == 0:
+                print(" 0", end="")
+
+            for drone_id in self.drones_delivered:
+
+                print(f" D{drone_id}", end="")
+
+            print("\n")
+
+        time.sleep(0.1)
